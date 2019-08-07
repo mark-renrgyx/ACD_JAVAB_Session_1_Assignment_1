d7b61b55d6b1e3b7ad778257ff2bed63adaa97a5
mark-renrgyx <53844281+mark-renrgyx@users.noreply.github.com>
2019-08-07 14:20:19 -0400

    Completed assignment
    1 source code file, and screenshot of success

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package renrgyx_training.week1;
+
+/**
+ * @author mark
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
